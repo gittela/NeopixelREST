66c808497c956012d23a8bd5f51826c673f3ccf2
gittela <howard@gittela.com>
2015-03-23 21:49:49 +0100

    minor edit

--- NeopixelREST.ino
+++ NeopixelREST.ino
@@ -6,60 +6,57 @@
 CRGB leds[NUM_LEDS];
 YunServer server;
 
-
 void setup() {
-  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  Serial.begin(9600);
-  Bridge.begin();
-  server.listenOnLocalhost();
-  server.begin();
-  leds[0-2] = CRGB::Black;
+	FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+	Serial.begin(9600);
+	Bridge.begin();
+	server.listenOnLocalhost();
+	server.begin();
+	leds[0 - 2] = CRGB::Black;
 
-  FastLED.show();
+	FastLED.show();
 }
 
 void loop() {
-  YunClient client = server.accept();
+	YunClient client = server.accept();
 
-  if (client) {
-    process(client);
-    client.stop();
-  }
+	if (client) {
+		process(client);
+		client.stop();
+	}
 
-  delay(50);
+	delay(50);
 }
 void process(YunClient client) {
 // read the command
-  String command = client.readStringUntil('/');
+	String command = client.readStringUntil('/');
 
-  if (command == "node") {
-    nodeCommand(client);
-  }
+	if (command == "node") {
+		nodeCommand(client);
+	}
 }
 void nodeCommand(YunClient client) {
-int number, color;
+	int number, color;
 // Read node number
-number = client.parseInt();
+	number = client.parseInt();
 
-if (client.read() == '/') {
-  color = client.parseInt();
-  if (color == 1){
-    leds[number] = CRGB::Red;
-  }
-  else if (color == 2){
-    leds[number] = CRGB::Blue;
-  }
-  else if (color == 3){
-    leds[number] = CRGB::Green;
-  }
-  FastLED.show();
-}
+	if (client.read() == '/') {
+		color = client.parseInt();
+		if (color == 1) {
+			leds[number] = CRGB::Red;
+		} else if (color == 2) {
+			leds[number] = CRGB::Blue;
+		} else if (color == 3) {
+			leds[number] = CRGB::Green;
+		}
+		FastLED.show();
+	}
 
 // Send feedback to client
-client.print(("Node ") + (String)number + (" set to ") + color);
+	client.print(("Node ") + (String) number + (" set to ") + color);
 
 // Update datastore key with the current pin value
-/*String key = "D";
-key += number;
-Bridge.put(key, String(color));*/
+	/*String key = "D";
+	 key += number;
+	 Bridge.put(key, String(color));*/
 }
